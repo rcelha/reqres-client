bf4d2779b2e130f094e68afe138feeee0476d412
Rodrigo Corrêa <rcelha@gmail.com>
2019-06-29 14:38:26 +0100

    Updates

---
+++ precache-manifest.bb5b048f901e0a5d58031fce592f2c01.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a5f105069449c61184f8c7b466033fc",
+    "url": "/reqres-client/index.html"
+  },
+  {
+    "revision": "75fd5eb37c8db9a8b4c5",
+    "url": "/reqres-client/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "b89d309b60a245123aee",
+    "url": "/reqres-client/static/js/2.c4546025.chunk.js"
+  },
+  {
+    "revision": "75fd5eb37c8db9a8b4c5",
+    "url": "/reqres-client/static/js/main.eaca906f.chunk.js"
+  },
+  {
+    "revision": "9e2ba28c7e9c22672c58",
+    "url": "/reqres-client/static/js/runtime~main.a152d097.js"
+  }
+]);
